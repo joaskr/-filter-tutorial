4a4a8561d4bcd42817361e07ea324306fd82669e
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2022-02-23 23:33:16 +0100

    Added logic to fetch data, display it and style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
 
 function App() {
+  const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=278d99b9f976d906b442dce7f7119fb0&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    setPopular(movies.results);
+  };
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="popular-movies">
+        {popular.map((movie) => {
+          return <Movie key={movie.id} movie={movie} />;
+        })}
+      </div>
     </div>
   );
 }
