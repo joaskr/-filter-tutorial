591873ac8e9d3720db06552dc9f155400fc28fec
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2022-02-23 23:32:57 +0100

    Created movie component

---
+++ src/Movie.js
@@ -0,0 +1,12 @@
+function Movie({ movie }) {
+  return (
+    <div>
+      <h2>{movie.title}</h2>
+      <img
+        src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
+        alt={movie.title}
+      ></img>
+    </div>
+  );
+}
+export default Movie;
