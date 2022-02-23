0737f16c17628f241f12886d8d9f604f0e9830be
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2022-02-24 00:04:12 +0100

    added filter logic and style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import Movie from "./Movie";
+import Filter from "./Filter";
 
 function App() {
   const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
   useEffect(() => {
     fetchPopular();
   }, []);
+
   const fetchPopular = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/popular?api_key=278d99b9f976d906b442dce7f7119fb0&language=en-US&page=1"
     );
     const movies = await data.json();
     setPopular(movies.results);
+    setFiltered(movies.results);
   };
 
   return (
     <div className="App">
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
       <div className="popular-movies">
-        {popular.map((movie) => {
+        {filtered.map((movie) => {
           return <Movie key={movie.id} movie={movie} />;
         })}
       </div>
